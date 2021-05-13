99077cad330ea169c5b19c066533df3241432995
Pawior <pawiorx@gmail.com>
2021-05-13 22:21:30 +0200

    files and structure creating

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../Home";
+const App = (): ReactElement => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
